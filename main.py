3689d9767eac4181407f01ad5b42f413d47ae43e
repl.it user <replituser@example.com>
2021-02-26 11:12:28 +0000

    NEW output

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print("hstg")
+print("1.git add .")
+print("2.git commit -m")
+print("3.git push")
---
+++ output/multiline.py
@@ -0,0 +1,7 @@
+#Display message to standart output in multiple lines of code
+print("System failure iminent!")
+print("System reboot has been initiated...")
+print("rebooting systems of beep..")
+print("reboting output motors")
+print("rebooting hover engine...")
+print("System is alive")
