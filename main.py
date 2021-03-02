349d282d2da168bd810bf59b09ebc21e92561096
repl.it user <replituser@example.com>
2021-03-02 11:27:52 +0000

    while loop

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
+# while loops and fot loops- repetition
 
-print("What is your name?")
-#primitive data types-string,integer,float,bool.
-n =str(input())
-print("A random name which is , true or false")
-name =input()
-
-if len(n) > 9 :
-  print("Long name")
-  print("you have {} characters ".format(len(n)))
-elif len(n) >= 4  :
-  print("The name is long")
-elif len(n) ==3 :
-  print("the name is GREAT")
-else:
-  print("less than 9 characters")
-
-print("end of this program")
+print("print symbol times")
+x= int(input())
+i=0;
+while i <x : #condition for reapiting the code 
+ print("\u2705",i)
+ i=i+1
+print("loop has been completed")
