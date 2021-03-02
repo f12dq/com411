7a3563933e4155c6739fb9723a0dbec540014b41
repl.it user <replituser@example.com>
2021-03-02 10:57:05 +0000

    new main data types

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-print("hstg")
-print("1.git add .")
-print("2.git commit -m")
-print("3.git push")
+print("What is your name?")
+#primitive data types-string,integer,float,bool.
+n =str(input())
+print("A random name which is , true or false")
+name =input()
+
+if len(n) > 9 and name == "true" :
+  print("Long name")
+  print("you have {} characters ".format(len(n)))
+else:
+  print("less than 9 characters")
+
+print("end of this program")
---
+++ output/demos/datatype.py
@@ -0,0 +1,10 @@
+print("What is your name?")
+#primitive data types-string,integer,float,bool.
+name =str(input())
+
+print("What is the age?")
+age =int(input())
+
+print("How much money do you have?")
+money =float(input())
+print(" welcome {}.\n You are {} years old.\n you have {} money left".format(name, age, money))
